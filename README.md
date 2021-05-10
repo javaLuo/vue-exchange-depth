3ee1ad9d96f135b110e6496178359c1b36562dae
javaLuo <376693576@qq.com>
2021-05-10 19:00:16 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# vue-exchange-depth [![npm](https://img.shields.io/npm/v/vue-exchange-depth.svg)](https://www.npmjs.com/package/vue-exchange-depth) [![npm downloads](https://img.shields.io/npm/dt/vue-exchange-depth.svg)](https://www.npmjs.com/package/vue-exchange-depth)
-
-交易所深度组件/挂单信息组件<br/>
-这个目前仅用于vue2.x
-
-### DEMO
-https://isluo.com/work/vue-exchange-depth
+这个分支存放的是DEMO页的源代码
--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-import Component from "./App.vue";
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
 
-export default Component;
+import { Checkbox, Radio } from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.config.productionTip = false;
+Vue.use(Checkbox);
+Vue.use(Radio);
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
